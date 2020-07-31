43b674a1c2d46b9757defe810c344d654a3eb45f
Brianna Keune <briannakeune@gmail.com>
2020-07-31 14:55:52 -0500

    set up config file to reach .env

---
+++ app.config.js
@@ -0,0 +1,6 @@
+export default {
+  url: process.env.REACT_APP_URL,
+  issuer: process.env.REACT_APP_ISSUER,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+};
