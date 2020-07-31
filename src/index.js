c79f516dd0544bdcd8a4c94448ece05e09b945a9
Brianna Keune <briannakeune@gmail.com>
2020-07-31 15:01:23 -0500

    app wrappers for okta and router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import { Security } from "@okta/okta-react";
+import {issuer, client_id, redirect_uri} from './app.config'
+
+const onAuthRequired = ({ history }) => history.push("/login");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Security
+        issuer={issuer}
+        client_id={client_id}
+        redirect_uri={redirect_uri}
+        onAuthRequired={onAuthRequired}
+      >
+        <App />
+      </Security>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
