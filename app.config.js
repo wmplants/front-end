1f3b03abd47aa86d2d9d5b1ac187b1a5c30a4172
Brianna Keune <briannakeune@gmail.com>
2020-07-31 15:00:34 -0500

    updated redirect to default

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 export default {
   url: process.env.REACT_APP_URL,
   issuer: process.env.REACT_APP_ISSUER,
-  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  redirect_uri: window.location.origin + "/implicit/callback",
   client_id: process.env.REACT_APP_CLIENT_ID,
 };
