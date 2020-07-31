37613964a84f3f80dab5a371ed49bba97fc78a9a
Brianna Keune <briannakeune@gmail.com>
2020-07-31 11:42:00 -0500

    react setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import LoginForm from './components/LoginForm.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
