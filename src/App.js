8e81e9bd67ee371b3a90f784d3ca457585a818dd
Brianna Keune <briannakeune@gmail.com>
2020-07-31 15:01:06 -0500

    app with routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import { SecureRoute, ImplicitCallBack } from "@okta/okta-react";
 
-import LoginForm from './components/LoginForm.js'
+import * as Component from "./components";
+import { url } from "./app.config.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <LoginForm />
-      </header>
+      {Component.NavBar}
+      <main>
+        <Route exact path="/" component={Component.HomePage} />
+        <Route path="/login" render={() => <Component.Login baseurl={url} />} />
+        <Route path="/implicit/callback" component={ImplicitCallBack} />
+        <Route
+          path="/register"
+          render={() => <Component.RegisterForm baseurl={url} />}
+        />
+        <SecureRoute path="/" component={Component.ProfilePage} />
+      </main>
     </div>
   );
 }
